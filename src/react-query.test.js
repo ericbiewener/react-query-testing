8b1bb972430b5cfb35c70775bda8a9e7d9f02501
Eric Biewener <eric.biewener0@walmart.com>
2022-11-09 12:56:46 -0800

    create react-query test

---
+++ src/react-query.test.js
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import { useQuery, QueryClientProvider, QueryClient } from "@tanstack/react-query"
+
+const queryCache = new QueryClient();
+
+const TestCmp = () => {
+  const result = useQuery(["foo"], () => Promise.resolve("foo"));
+  console.info(`::`, result);
+  return null;
+};
+
+test('should have no act() error', () => {
+  render(
+    <QueryClientProvider client={queryCache}>
+      <TestCmp />
+    </QueryClientProvider>
+  );
+});
