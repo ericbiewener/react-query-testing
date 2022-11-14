8d94dca6cb5a4be5e916b35d1f3a373fa910b7e0
Eric Biewener <eric.biewener0@walmart.com>
2022-11-14 14:45:13 -0800

    more - 2:45 pm - Nov 14

--- src/react-query.test.js
+++ src/react-query.test.js
@@ -1,22 +1,60 @@
-import { render } from "@testing-library/react";
+import { useState, useEffect } from "react";
+import { render, screen } from "@testing-library/react";
 import {
   useQuery,
   QueryCache,
   ReactQueryCacheProvider,
+  useMutation,
 } from "react-query";
 
 const queryCache = new QueryCache();
 
-const TestCmp = () => {
-  const result = useQuery(["foo"], () => Promise.resolve("foo"));
-  console.info(`::`, result);
+const QueryCmp = ({ queryFn }) => {
+  useQuery(["foo"], queryFn);
   return null;
+};
+
+const MutationCmp = ({ mutationFn }) => {
+  const [isDone, setIsDone] = useState(false);
+
+  const [mutate] = useMutation(mutationFn, {
+    useErrorBoundary: false,
+    onSuccess: (...args) => console.log(":: onSuccess", args),
+    onError: (...args) => console.log(":: onError", args),
+  });
+
+  useEffect(() => {
+    mutate()
+      .then((rsp) => console.log(":: rsp", rsp))
+      .catch((e) => console.error(":: e", e))
+      .finally(() => setIsDone(true));
+  }, []);
+
+  return isDone ? <div>done</div> : null;
 };
 
 test("should have no act() error", () => {
   render(
     <ReactQueryCacheProvider queryCache={queryCache}>
-      <TestCmp />
+      <QueryCmp queryFn={() => Promise.resolve("foo")} />
     </ReactQueryCacheProvider>
   );
+});
+
+test("rejected promise testing - query", () => {
+  render(
+    <ReactQueryCacheProvider queryCache={queryCache}>
+      <QueryCmp queryFn={() => Promise.reject("foo")} />
+    </ReactQueryCacheProvider>
+  );
+});
+
+test("rejected promise testing - mutations", async () => {
+  render(
+    <ReactQueryCacheProvider queryCache={queryCache}>
+      <MutationCmp mutationFn={() => Promise.reject("foo")} />
+    </ReactQueryCacheProvider>
+  );
+
+  await screen.findByText("done");
 });
