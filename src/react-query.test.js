6bec3ef4ac6bb29d3e3f1e0959eb7cc9c4de4354
Eric Biewener <eric.biewener0@walmart.com>
2022-11-14 12:06:10 -0800

    more - 12:06 pm - Nov 14

--- src/react-query.test.js
+++ src/react-query.test.js
@@ -1,18 +1,57 @@
-import { render } from '@testing-library/react';
-import { useQuery, QueryClientProvider, QueryClient } from "@tanstack/react-query"
+import { useEffect, useState } from 'react'
+import { render, screen } from "@testing-library/react";
+import {
+  useQuery,
+  QueryClientProvider,
+  QueryClient,
+  useMutation,
+} from "@tanstack/react-query";
 
 const queryCache = new QueryClient();
 
-const TestCmp = () => {
-  const result = useQuery(["foo"], () => Promise.resolve("foo"));
-  console.info(`::`, result);
+const QueryCmp = ({ queryFn }) => {
+  const result = useQuery(["foo"], queryFn);
   return null;
 };
 
-test('should have no act() error', () => {
+const MutationCmp = ({ mutationFn }) => {
+  const [isDone, setIsDone] = useState(false)
+
+  const result = useMutation(mutationFn, {
+    useErrorBoundary: false,
+    onSuccess: (...args) => console.log(":: onSuccess", args),
+    onError: (...args) => console.log(":: onError", args),
+  });
+
+  useEffect(() => {
+    result.mutateAsync().finally(() => setIsDone(true))
+  }, [])
+
+  return isDone ? <div>done</div> : null;
+};
+
+test("should have no act() error", () => {
   render(
     <QueryClientProvider client={queryCache}>
-      <TestCmp />
+      <QueryCmp queryFn={() => Promise.resolve("foo")} />
     </QueryClientProvider>
   );
+});
+
+test("rejected promise teseting - query", () => {
+  render(
+    <QueryClientProvider client={queryCache}>
+      <QueryCmp queryFn={() => Promise.reject("foo")} />
+    </QueryClientProvider>
+  );
+});
+
+test("rejected promise teseting - mutations", async () => {
+  render(
+    <QueryClientProvider client={queryCache}>
+      <MutationCmp mutationFn={() => Promise.reject("foo")} />
+    </QueryClientProvider>
+  );
+
+  await screen.findByText('done')
 });
