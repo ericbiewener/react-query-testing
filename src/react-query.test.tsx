3cd1721dc0ce8c516447969a25e93b43d617f229
Eric Biewener <eric.biewener0@walmart.com>
2022-11-15 06:54:56 -0800

    more - 6:54 am - Nov 15

--- src/react-query.test.tsx
+++ src/react-query.test.tsx
@@ -1,33 +1,54 @@
-import { useState, useEffect } from "react";
-import { render, screen } from "@testing-library/react";
+import React, { useState, useEffect, FC } from "react";
+import { render, screen, act } from "@testing-library/react";
 import {
   useQuery,
-  QueryCache,
+  makeQueryCache,
   ReactQueryCacheProvider,
   useMutation,
 } from "react-query";
 
-const queryCache = new QueryCache();
+const consoleError = console.error
+console.error = (...args: any[]) => {
+  if (args[0].includes('was not wrapped in act')) return
+  consoleError.apply(console, args)
+}
+
+afterEach(async () => {
+  await act(() => Promise.resolve())
+})
+
+
+const queryCache = makeQueryCache();
 
 const QueryCmp = ({ queryFn }) => {
   useQuery(["foo"], queryFn);
   return null;
 };
 
-const MutationCmp = ({ mutationFn }) => {
+const MutationCmp: FC<{ mutationFn: () => Promise<{ foo: 1 }> }> = ({
+  mutationFn,
+}) => {
   const [isDone, setIsDone] = useState(false);
 
   const [mutate] = useMutation(mutationFn, {
-    useErrorBoundary: false,
+    // throwOnError: true,
     onSuccess: (...args) => console.log(":: onSuccess", args),
     onError: (...args) => console.log(":: onError", args),
   });
 
   useEffect(() => {
-    mutate()
-      .then((rsp) => console.log(":: rsp", rsp))
-      .catch((e) => console.error(":: e", e))
-      .finally(() => setIsDone(true));
+    const go = async () => {
+      try {
+        const rsp = await mutate();
+        console.log(":: RESPONSE", rsp);
+      } catch (e) {
+        console.error(":: e", e);
+      } finally {
+        setIsDone(true);
+      }
+    };
+
+    go();
   }, []);
 
   return isDone ? <div>done</div> : null;
